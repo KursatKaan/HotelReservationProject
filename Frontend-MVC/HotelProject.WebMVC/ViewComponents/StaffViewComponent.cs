e1d4925552476b5acb818f292a520cb5630f93e1
Kürşat Kaan <140398297+KursatKaan@users.noreply.github.com>
2024-06-12 17:41:25 +0300

    Added Registiration

---
+++ Frontend-MVC/HotelProject.WebMVC/ViewComponents/StaffViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.ViewComponents
+{
+    public class StaffViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
